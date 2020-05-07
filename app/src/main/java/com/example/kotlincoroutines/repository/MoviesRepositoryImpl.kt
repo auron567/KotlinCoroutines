3e041d5868128d1b33dc015cf0579c1e0d5e084f
auron567 <xauron567x@gmail.com>
2020-05-07 10:23:03 +0200

    Use Retrofit coroutines support

--- app/src/main/java/com/example/kotlincoroutines/repository/MoviesRepositoryImpl.kt
+++ app/src/main/java/com/example/kotlincoroutines/repository/MoviesRepositoryImpl.kt
@@ -9,7 +9,6 @@ import com.example.kotlincoroutines.data.network.MoviesService
 import com.example.kotlincoroutines.di.API_KEY
 import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.supervisorScope
 import kotlinx.coroutines.withContext
 import timber.log.Timber
 import java.io.IOException
@@ -20,41 +19,31 @@ class MoviesRepositoryImpl(
     private val contextProvider: CoroutineContextProvider
 ) : MoviesRepository {
 
-    @Suppress("BlockingMethodInNonBlockingContext")
     override suspend fun getMovies(): Result<List<Movie>> = withContext(contextProvider.context) {
-        supervisorScope {
+        Timber.d(coroutineLog)
+
+        val savedMoviesDeferred = async {
             Timber.d(coroutineLog)
+            movieDao.getMovies()
+        }
 
-            val savedMoviesDeferred = async {
-                Timber.d(coroutineLog)
-                movieDao.getMovies()
-            }
-            val responseDeferred = async {
-                Timber.d(coroutineLog)
-                moviesService.getMovies(API_KEY).execute()
+        try {
+            val response = moviesService.getMovies(API_KEY)
+            val movies = response.movies.also {
+                launch {
+                    Timber.d(coroutineLog)
+                    movieDao.saveMovies(it)
+                }
             }
 
+            Result(movies, null)
+        } catch (throwable: Throwable) {
             val savedMovies = savedMoviesDeferred.await()
-            try {
-                val response = responseDeferred.await()
-                val movies = response.body()?.movies?.also {
-                    launch {
-                        Timber.d(coroutineLog)
-                        movieDao.saveMovies(it)
-                    }
-                }
 
-                if (response.isSuccessful && movies != null) {
-                    Result(movies, null)
-                } else {
-                    Result(savedMovies, null)
-                }
-            } catch (throwable: Throwable) {
-                if (throwable is IOException && savedMovies.isNotEmpty()) {
-                    Result(savedMovies, null)
-                } else {
-                    Result(null, throwable)
-                }
+            if (throwable is IOException && savedMovies.isNotEmpty()) {
+                Result(savedMovies, null)
+            } else {
+                Result(null, throwable)
             }
         }
     }
